ce033796834abf9a69c956fbbf1907179caa1c42
Earn1ll <coldsteelblr@gmail.com>
2023-01-19 18:30:29 +0300

    feat: add colors.scss

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function Header() {
   return (
     <header className="header">
 
       <nav className="nav">
-        <Link to="/garage" className="nav-item">
-          Garage
-        </Link>
-        <Link to="/winners" className="nav-item">
-          Winners
-        </Link>
+      <NavLink to="/garage" className="nav-item">
+          <div>garage</div>
+          </NavLink>
+        <NavLink to="/winners" className="nav-item">
+          <div>Winners</div>
+          </NavLink>
       </nav>
     </header>
   );
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './Main.scss';
 import Garage from '../Garage/Garage';
 import NotFoundPage from '../NotFound/NotFound';
